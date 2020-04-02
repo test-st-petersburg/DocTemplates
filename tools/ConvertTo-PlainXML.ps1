4c0da83a02ed853de933f01c0d9168732f1e82e3
sergey.s.betke <sergey.s.betke@yandex.ru>
2020-04-02 16:12:54 +0300

    build(design): Добавить задачу разборки ott в vscode
    МЕТАДАННЫЕ:#1

---
+++ tools/ConvertTo-PlainXML.ps1
@@ -0,0 +1,33 @@
+﻿<#
+	.SYNOPSIS
+		Преобразовать open office файлов в папки с xml файлами для последующего
+		хранения в git репозиториях
+#>
+
+#Requires -Version 5.0
+
+[CmdletBinding()]
+param(
+	#
+	[Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $false)]
+	[Alias("ODT_File")]
+	[Alias("ODTFile")]
+	[Alias("OTT_File")]
+	[Alias("OTTFile")]
+	[System.String]
+	$FilePath,
+
+	[Parameter(Mandatory = $True, Position = 1, ValueFromPipeline = $false)]
+	[Alias("Directory")]
+	[System.String]
+	$DestinationPath,
+
+	# форматировать структуру xml файлов
+	[Parameter()]
+	[Switch]
+	$Indented
+)
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
+
+Expand-Archive -Path $FilePath -DestinationPath $DestinationPath;
