7ffae84c1e6d6892a07714b194ec60a10a3e3c16
sergey.s.betke <sergey.s.betke@yandex.ru>
2020-04-02 17:29:34 +0300

    build(design): Enable verbose and debug for pack/unpack tasks
    МЕТАДАННЫЕ:#1

--- tools/ConvertTo-PlainXML.ps1
+++ tools/ConvertTo-PlainXML.ps1
@@ -6,19 +6,19 @@
 
 #Requires -Version 5.0
 
-[CmdletBinding()]
+[CmdletBinding( ConfirmImpact = 'Low', SupportsShouldProcess = $true )]
 param(
 	#
-	[Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $false)]
-	[Alias("ODT_File")]
-	[Alias("ODTFile")]
-	[Alias("OTT_File")]
-	[Alias("OTTFile")]
+	[Parameter( Mandatory = $True, Position = 0, ValueFromPipeline = $false )]
+	[Alias( "ODT_File" )]
+	[Alias( "ODTFile" )]
+	[Alias( "OTT_File" )]
+	[Alias( "OTTFile" )]
 	[System.String]
 	$FilePath,
 
-	[Parameter(Mandatory = $True, Position = 1, ValueFromPipeline = $false)]
-	[Alias("Directory")]
+	[Parameter( Mandatory = $True, Position = 1, ValueFromPipeline = $false )]
+	[Alias( "Directory" )]
 	[System.String]
 	$DestinationPath,
 
@@ -30,4 +30,17 @@ param(
 
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
 
-Expand-Archive -Path $FilePath -DestinationPath $DestinationPath;
+if ( $PSCmdlet.ShouldProcess( $FilePath, "Convert Open Office document to plain XML directory" ) ) {
+	$TempZIPFileName = [System.IO.Path]::GetTempFileName() + '.zip';
+	Copy-Item -Path $FilePath -Destination $TempZIPFileName `
+		-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+		-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
+	$DestinationDirName = ( Get-Item $FilePath ).Name;
+	$DestinationPathForFile = Join-Path -Path $DestinationPath -ChildPath $DestinationDirName;
+	Expand-Archive -Path $TempZIPFileName -DestinationPath $DestinationPathForFile `
+		-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+		-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
+	Remove-Item -Path $TempZIPFileName `
+		-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+		-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
+};
