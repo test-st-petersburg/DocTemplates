2d7fcd30c6dd5df5ec0b576547650bc0ac314e38
sergey.s.betke <sergey.s.betke@yandex.ru>
2020-04-02 19:46:06 +0300

    feat(design): Add XML formatting for odt unpacking
    МЕТАДАННЫЕ:#1

--- tools/ConvertTo-PlainXML.ps1
+++ tools/ConvertTo-PlainXML.ps1
@@ -43,4 +43,36 @@ if ( $PSCmdlet.ShouldProcess( $FilePath, "Convert Open Office document to plain 
 	Remove-Item -Path $TempZIPFileName `
 		-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
 		-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
+
+	if ( $Indented ) {
+		if ( $PSCmdlet.ShouldProcess( $DestinationPathForFile, "Format all xml files in Open Office document plain XML directory" ) ) {
+			[System.Xml.XmlReaderSettings] $readerSettings = New-Object System.Xml.XmlReaderSettings;
+			$readerSettings.ConformanceLevel = [System.Xml.ConformanceLevel]::Document;
+			$readerSettings.DtdProcessing = [System.Xml.DtdProcessing]::Parse;
+			# не обрабатывать DTD в XML документах
+			$readerSettings.XmlResolver = $null;
+			$readerSettings.IgnoreComments = $false;
+			$readerSettings.IgnoreProcessingInstructions = $false;
+			$readerSettings.IgnoreWhitespace = $false;
+			$readerSettings.CloseInput = $true;
+			Get-ChildItem -Path $DestinationPathForFile -Filter '*.xml' -Recurse `
+			| Where-Object { $_.Length -gt 0 } `
+			| ForEach-Object {
+				Write-Debug "Read xml file $($_.FullName)";
+				[System.Xml.XmlDocument] $xml = New-Object System.Xml.XmlDocument;
+				[System.Xml.XmlReader] $xmlReader = [System.Xml.XmlReader]::Create( $_.FullName, $readerSettings );
+				$xml.PreserveWhitespace = true;
+				$xml.XmlResolver = $null;
+				$xml.Load( $xmlReader );
+				$xmlReader.Close();
+				Write-Verbose "Rewrite (format) xml file $($_.FullName)";
+				[System.Xml.XmlTextWriter] $xmlWriter = New-Object System.Xml.XmlTextWriter( $_.FullName, $Null );
+				$xmlWriter.Formatting = [System.Xml.Formatting]::Indented;
+				$xmlWriter.Indentation = 1;
+				$xmlWriter.IndentChar = "`t";
+				$xml.WriteContentTo( $xmlWriter );
+				$xmlWriter.Flush();
+			};
+		};
+	};
 };
