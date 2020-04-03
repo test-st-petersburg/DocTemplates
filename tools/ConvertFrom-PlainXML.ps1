2f29b2807e6a64017eeff8e47964a7ab2f48b0ff
sergey.s.betke <sergey.s.betke@yandex.ru>
2020-04-03 19:21:47 +0300

    build(design): Write to .ott first mimetype without compression
    МЕТАДАННЫЕ:#1

--- tools/ConvertFrom-PlainXML.ps1
+++ tools/ConvertFrom-PlainXML.ps1
@@ -30,20 +30,95 @@ param(
 
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
 
-if ( $PSCmdlet.ShouldProcess( $FilePath, "Create Open Office document from plain XML directory" ) ) {
+function Use-Object {
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true)]
+		[AllowEmptyString()]
+		[AllowEmptyCollection()]
+		[AllowNull()]
+		[Object]
+		$InputObject,
+
+		[Parameter(Mandatory = $true)]
+		[scriptblock]
+		$ScriptBlock
+	)
+
+	try	{
+		. $ScriptBlock;
+	}
+ finally {
+		if ($null -ne $InputObject -and $InputObject -is [System.IDisposable]) {
+			$InputObject.Dispose();
+		};
+	};
+}
+
+if ( $PSCmdlet.ShouldProcess( $DestinationPath, "Create Open Office document from plain XML directory" ) ) {
+	Add-Type -AssemblyName System.IO;
+	Add-Type -AssemblyName System.IO.Compression;
+	Add-Type -AssemblyName System.IO.Compression.FileSystem;
+
 	$TempZIPFileName = [System.IO.Path]::GetTempFileName() + '.zip';
-	Compress-Archive -Path ( Join-Path -Path $Path -ChildPath 'mimetype' ) `
-		-DestinationPath $TempZIPFileName  `
-		-CompressionLevel NoCompression `
-		-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
-		-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
-	Get-ChildItem -Path $Path -Exclude 'mimetype' `
-	| Compress-Archive `
-		-DestinationPath $TempZIPFileName `
-		-Update `
-		-CompressionLevel Fastest `
-		-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
-		-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
+
+	Write-Verbose "Create and open zip file $TempZIPFileName";
+	Use-Object ( $zipFileStream = New-Object System.IO.FileStream( $TempZIPFileName, [System.IO.FileMode]::Create ) ) {
+		Use-Object ( $archive = New-Object System.IO.Compression.ZipArchive( $zipFileStream, [System.IO.Compression.ZipArchiveMode]::Create ) ) {
+			Push-Location;
+			Set-Location $Path;
+			# по требованиям Open Office первым добавляем **без сжатия** файл mimetype
+			@( Get-Item -Path 'mimetype' ) +
+			@( Get-ChildItem -Path . -File -Recurse -Exclude 'mimetype' ) `
+				| ForEach-Object {
+				$entryPath = ( Resolve-Path -Path $_ -Relative ).Substring( 2 );
+				if ( $entryPath -in , 'mimetype' ) {
+					$compressionLevel = [System.IO.Compression.CompressionLevel]::NoCompression;
+				}
+				else {
+					$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal;
+				};
+				Write-Debug "Create archive entry $entryPath with compression level $compressionLevel";
+				$entry = $archive.CreateEntry( $entryPath, $compressionLevel );
+				# $entry.ExternalAttributes = 0;
+				Use-Object ( $entryStream = $entry.Open() ) {
+					Write-Debug "Open source file $_.FullName";
+					Use-Object ( $fileStream = [System.IO.File]::Open( $_.FullName, [System.IO.FileMode]::Open ) ) {
+						Write-Verbose "Add file $entryPath to archive $TempZIPFileName with compression level $compressionLevel";
+						$fileStream.CopyTo( $entryStream );
+					};
+				};
+			};
+
+			# $mimetypeEntry = $archive.CreateEntry( 'mimetype', [System.IO.Compression.CompressionLevel]::NoCompression );
+			# $mimetypeEntry.ExternalAttributes = 0;
+			# Use-Object ( $entryStream = $mimetypeEntry.Open() ) {
+			# 	Use-Object ( $fileStream = [System.IO.File]::Open( ( Join-Path -Path $Path -ChildPath 'mimetype' ), [System.IO.FileMode]::Open ) ) {
+			# 		$fileStream.CopyTo( $entryStream );
+			# 	};
+			# };
+			# [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile( $archive,
+			# 	( Join-Path -Path $Path -ChildPath 'mimetype' ),
+			# 	'mimetype',
+			# 	[System.IO.Compression.CompressionLevel]::NoCompression
+			# );
+
+			Pop-Location;
+		};
+	};
+
+	# Compress-Archive -Path ( Join-Path -Path $Path -ChildPath 'mimetype' ) `
+	# 	-DestinationPath $TempZIPFileName  `
+	# 	-CompressionLevel NoCompression `
+	# 	-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+	# 	-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
+	# Get-ChildItem -Path $Path -Exclude 'mimetype' `
+	# | Compress-Archive `
+	# 	-DestinationPath $TempZIPFileName `
+	# 	-Update `
+	# 	-CompressionLevel Fastest `
+	# 	-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+	# 	-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
 	Move-Item -Path $TempZIPFileName -Destination $DestinationPath `
 		-Force:( $PSCmdlet.MyInvocation.BoundParameters.Force.IsPresent -eq $true ) `
 		-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
