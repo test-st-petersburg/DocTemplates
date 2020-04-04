749581ddeafad2c9ae581febd21dbc43c147b28b
Sergey S. Betke <sergey.s.betke@yandex.ru>
2020-04-04 12:58:48 +0300

    build(design): Add ConvertFrom-PlainXMLAllDocument script
    МЕТАДАННЫЕ:#1

--- tools/ConvertFrom-PlainXML.ps1
+++ tools/ConvertFrom-PlainXML.ps1
@@ -9,7 +9,7 @@
 [CmdletBinding( ConfirmImpact = 'Low', SupportsShouldProcess = $true )]
 param(
 	# путь к папке с xml файлами
-	[Parameter( Mandatory = $True, Position = 0, ValueFromPipeline = $false )]
+	[Parameter( Mandatory = $True, Position = 0, ValueFromPipeline = $true )]
 	[Alias( "Directory" )]
 	[System.String]
 	$Path,
@@ -29,26 +29,29 @@ param(
 	$Force
 )
 
-$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
-
-Import-Module 7Zip4Powershell;
-
-if ( $PSCmdlet.ShouldProcess( $DestinationPath, "Create Open Office document from plain XML directory" ) ) {
-	$TempZIPFileName = [System.IO.Path]::GetTempFileName() + '.zip';
-	# по требованиям Open Office первым добавляем **без сжатия** файл mimetype
-	Compress-7Zip -ArchiveFileName $TempZIPFileName -Path $Path -Filter 'mimetype' `
-		-Format Zip `
-		-CompressionLevel None `
-		-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
-		-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
-	Get-ChildItem -Path $Path -File -Recurse -Exclude 'mimetype' `
-	| Compress-7Zip -ArchiveFileName $TempZIPFileName -Append `
-		-Format Zip `
-		-CompressionLevel Normal -CompressionMethod Deflate `
-		-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
-		-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
-	Move-Item -Path $TempZIPFileName -Destination $DestinationPath `
-		-Force:( $PSCmdlet.MyInvocation.BoundParameters.Force.IsPresent -eq $true ) `
-		-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
-		-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
-};
+begin {
+	$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
+	Import-Module 7Zip4Powershell;
+}
+process {
+	if ( $PSCmdlet.ShouldProcess( $DestinationPath, "Create Open Office document from plain XML directory" ) ) {
+		$TempZIPFileName = [System.IO.Path]::GetTempFileName() + '.zip';
+		# по требованиям Open Office первым добавляем **без сжатия** файл mimetype
+		Compress-7Zip -ArchiveFileName $TempZIPFileName -Path $Path -Filter 'mimetype' `
+			-Format Zip `
+			-CompressionLevel None `
+			-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+			-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
+		Get-ChildItem -Path $Path -File -Recurse -Exclude 'mimetype' `
+		| Compress-7Zip -ArchiveFileName $TempZIPFileName -Append `
+			-Format Zip `
+			-CompressionLevel Normal -CompressionMethod Deflate `
+			-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+			-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
+		Move-Item -Path $TempZIPFileName `
+			-Destination ( Join-Path -Path $DestinationPath -ChildPath ( ( Get-Item -Path $Path ).BaseName ) ) `
+			-Force:( $PSCmdlet.MyInvocation.BoundParameters.Force.IsPresent -eq $true ) `
+			-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+			-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
+	};
+}
---
+++ tools/ConvertFrom-PlainXMLAllDocuments.ps1
@@ -0,0 +1,34 @@
+﻿<#
+	.SYNOPSIS
+		Создаёт open office файлы из папки с xml файлами
+#>
+
+#Requires -Version 5.0
+
+[CmdletBinding( ConfirmImpact = 'Low', SupportsShouldProcess = $true )]
+param(
+	# путь к папке с xml файлами
+	[Parameter( Mandatory = $True, Position = 0, ValueFromPipeline = $false )]
+	[System.String]
+	$Path,
+
+	# путь к файлу Open Office, который будет создан
+	[Parameter( Mandatory = $True, Position = 1, ValueFromPipeline = $false )]
+	[System.String]
+	$DestinationPath,
+
+	# перезаписать существующий файл
+	[Parameter()]
+	[Switch]
+	$Force
+)
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
+
+Get-ChildItem -Path $Path `
+| Resolve-Path -Relative `
+| . ( Join-Path -Path $PSScriptRoot -ChildPath 'ConvertFrom-PlainXML.ps1' ) `
+	-DestinationPath $DestinationPath `
+	-Force:( $PSCmdlet.MyInvocation.BoundParameters.Force.IsPresent -eq $true ) `
+	-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+	-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
