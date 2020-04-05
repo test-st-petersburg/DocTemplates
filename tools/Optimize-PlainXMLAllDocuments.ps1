00eba8ad03bb4a2c3b8a345509c2973875f927ae
Sergey S. Betke <sergey.s.betke@yandex.ru>
2020-04-05 12:49:41 +0300

    build(design): Add Optimize content.xml task and script
    МЕТАДАННЫЕ:#4

---
+++ tools/Optimize-PlainXMLAllDocuments.ps1
@@ -0,0 +1,25 @@
+﻿<#
+	.SYNOPSIS
+		Удаляет мусур в content.xml файлах open office документов
+#>
+
+#Requires -Version 5.0
+
+[CmdletBinding( ConfirmImpact = 'Medium', SupportsShouldProcess = $true )]
+param(
+	# путь к папке, в которой созданы папки с xml файлами
+	[Parameter( Mandatory = $True, Position = 0, ValueFromPipeline = $true )]
+	[System.String]
+	$Path
+)
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
+
+Get-ChildItem -Path $Path -Directory `
+| Resolve-Path -Relative `
+| Get-ChildItem -Filter 'content.xml' `
+| Resolve-Path -Relative `
+| . ( Join-Path -Path $PSScriptRoot -ChildPath 'Optimize-PlainXML.ps1' ) `
+	-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+	-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true ) `
+	-WhatIf:( $PSCmdlet.MyInvocation.BoundParameters.WhatIf.IsPresent -eq $true );
