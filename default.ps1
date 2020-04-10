965861132b8e4363c1de349518145cad3ec0425c
sergey.s.betke <sergey.s.betke@yandex.ru>
2020-04-10 18:43:37 +0300

    refactor(design): Use psake for VSCode clean task

---
+++ default.ps1
@@ -0,0 +1,14 @@
+﻿#Requires -Version 5.0
+#Requires -Modules psake
+
+# task default -depends Build;
+
+task Clean {
+	if ( Test-Path -Path 'src' ) {
+		Remove-Item -Path 'src' -Recurse -Force;
+	};
+}
+
+task ? -Description "Helper to display task info" {
+	Write-Documentation;
+};
