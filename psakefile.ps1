891eafce60c2b209ed6a8de399e34eaf57cb6f79
sergey.s.betke <sergey.s.betke@yandex.ru>
2020-04-10 20:05:34 +0300

    fix(build): Fix Clean script error when files does not exists

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,10 +1,27 @@
 ﻿#Requires -Version 5.0
 #Requires -Modules psake
 
+properties {
+	$buildOutputPath = ".\template";
+	$sourcePath = ".\src\template";
+	$ottFiles = ( Get-ChildItem -Path $buildOutputPath -File -Filter '*.ott' );
+	$sources = ( Get-ChildItem -Path $sourcePath -Directory );
+};
+
 # task default -depends Build;
 
-task Clean {
-	if ( Test-Path -Path 'src' ) {
-		Remove-Item -Path 'src' -Recurse -Force;
+task Clean -Description 'Clean source directory for selected or all .ott files' {
+	Write-Host $sources;
+	Push-Location;
+	try {
+		Set-Location $sourcePath;
+		$sources | ForEach-Object {
+			if ( Test-Path -Path $_ ) {
+				Remove-Item -Path $_ -Recurse -Force;
+			};
+		};
+	}
+	finally {
+		Pop-Location;
 	};
-}
+};
