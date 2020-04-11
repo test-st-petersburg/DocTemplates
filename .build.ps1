62ad5af91cda2ceb2f7deb6a5fc8abc34ce88d10
Sergey S. Betke <sergey.s.betke@yandex.ru>
2020-04-11 03:26:55 +0300

    feat(build): Build just outdated targets (OO files)
    МЕТАДАННЫЕ:#17

--- .build.ps1
+++ .build.ps1
@@ -16,7 +16,7 @@ param(
 	# путь к .ott файлу
 	[Parameter( Mandatory = $false )]
 	[System.String[]]
-	$DestinationFile = ( ( Get-ChildItem -Path $DestinationPath -File -Filter '*.ott' ) | Resolve-Path ),
+	$DestinationFile = @( ( Get-ChildItem -Path $DestinationPath -File -Filter '*.ott' ) | Resolve-Path ),
 
 	# путь к папке с xml папками .ott файлов
 	[Parameter( Mandatory = $false, Position = 1 )]
@@ -26,7 +26,7 @@ param(
 	# путь к папке с xml файлами одного .ott файла
 	[Parameter( Mandatory = $false )]
 	[System.String[]]
-	$XMLFolder = ( ( Get-ChildItem -Path $Path -Directory -Filter '*.ott' ) | Resolve-Path )
+	$XMLFolder = @( ( Get-ChildItem -Path $Path -Directory -Filter '*.ott' ) | Resolve-Path )
 )
 
 # Synopsis: Удаляет каталоги с XML файлами
@@ -34,14 +34,6 @@ task Clean {
 	$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
 	$XMLFolder | Where-Object { $_ } | Where-Object { Test-Path -Path $_ } | Remove-Item -Recurse -Force;
 };
-
-# task Build - `
-# 	-Outputs @() `
-# 	-Inputs @() `
-# {
-# 	process {
-# 	}
-# }
 
 # Synopsis: Преобразовывает Open Office файлы в папки с XML файлами
 task Unpack Clean, {
@@ -61,11 +53,31 @@ task OptimizeXML {
 };
 
 # Synopsis: Создаёт Open Office файлы из папки с XML файлами (build)
-task Build {
-	$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
-	$XMLFolder | .\tools\ConvertFrom-PlainXML.ps1 -DestinationPath $DestinationPath -Force `
-		-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
-		-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
+
+$OOFilesBuildTasks = @();
+
+foreach ( $documentXMLFolder in $XMLFolder ) {
+	$documentName = $( Split-Path -Path ( $DocumentXMLFolder ) -Leaf );
+	$OOFilesBuildTask = "Build $documentName";
+	$OOFilesBuildTasks += $OOFilesBuildTask;
+	$prerequisites = @( Get-ChildItem -Path $documentXMLFolder -File -Recurse );
+	$target = @( Join-Path -Path $DestinationPath -ChildPath $documentName );
+
+	# dynamically added incremental task
+	task $OOFilesBuildTask `
+		-Inputs $prerequisites `
+		-Outputs $target `
+	{
+		$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
+		$localDestinationFile = $Outputs[0];
+		$localXMLFolder = @( Join-Path -Path $Path -ChildPath ( Split-Path -Path $localDestinationFile -Leaf ) );
+		$localXMLFolder | .\tools\ConvertFrom-PlainXML.ps1 -DestinationPath $DestinationPath -Force `
+			-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+			-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true );
+	};
 };
+
+# Synopsis: Создаёт Open Office файлы из папки с XML файлами (build)
+task Build $OOFilesBuildTasks;
 
 task . Build;
