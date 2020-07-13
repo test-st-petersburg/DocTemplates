89308531ece2f5f01808faa8b479b6c0f8861a9c
Sergey S. Betke <sergey.s.betke@yandex.ru>
2020-07-13 11:46:27 +0300

    "fix(build): Fix Update-FileLastWriteTime.ps1"

--- tools/build/Update-FileLastWriteTime.ps1
+++ tools/build/Update-FileLastWriteTime.ps1
@@ -8,25 +8,23 @@
 	.Parameter Path
 		The path of the file to create or update.
 #>
-Function Update-FileLastWriteTime {
-	[CmdletBinding( ConfirmImpact = 'Low', SupportsShouldProcess = $true )]
-	Param(
-		[Parameter( Mandatory = $True, Position = 1 )]
-		[System.String] $Path
-	)
+[CmdletBinding( ConfirmImpact = 'Low', SupportsShouldProcess = $true )]
+Param(
+	[Parameter( Mandatory = $True, Position = 1 )]
+	[System.String] $Path
+)
 
-	if ( -not [System.Management.Automation.WildcardPattern]::ContainsWildcardCharacters( $Path ) ) {
-		if ( -not ( Test-Path -Path $Path ) ) {
-			New-Item -ItemType File -Path $Path `
-				-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
-				-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true ) `
-				-WhatIf:( $PSCmdlet.MyInvocation.BoundParameters.WhatIf.IsPresent -eq $true ) `
-			| Out-Null;
-		};
+if ( -not [System.Management.Automation.WildcardPattern]::ContainsWildcardCharacters( $Path ) ) {
+	if ( -not ( Test-Path -Path $Path ) ) {
+		New-Item -ItemType File -Path $Path `
+			-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+			-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true ) `
+			-WhatIf:( $PSCmdlet.MyInvocation.BoundParameters.WhatIf.IsPresent -eq $true ) `
+		| Out-Null;
 	};
-	Set-ItemProperty -Path $Path -Name LastWriteTime -Value ( Get-Date ) `
-		-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
-		-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true ) `
-		-WhatIf:( $PSCmdlet.MyInvocation.BoundParameters.WhatIf.IsPresent -eq $true ) `
-	| Out-Null;
-}
+};
+Set-ItemProperty -Path $Path -Name LastWriteTime -Value ( Get-Date ) `
+	-Verbose:( $PSCmdlet.MyInvocation.BoundParameters.Verbose.IsPresent -eq $true ) `
+	-Debug:( $PSCmdlet.MyInvocation.BoundParameters.Debug.IsPresent -eq $true ) `
+	-WhatIf:( $PSCmdlet.MyInvocation.BoundParameters.WhatIf.IsPresent -eq $true ) `
+| Out-Null;
