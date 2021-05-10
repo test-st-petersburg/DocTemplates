9e33b248decf3f4ea624dfc3bbd736e4a0595ff7
Sergey S. Betke <sergey.s.betke@yandex.ru>
2021-05-10 23:40:19 +0300

    feat(vcard): :construction: Prepare ConverTo-vCard
    #118

---
+++ tools/xCard/ConvertTo-vCard.ps1
@@ -0,0 +1,47 @@
+﻿# Copyright © 2021 Sergei S. Betke
+
+#Requires -Version 5.0
+
+<#
+	.SYNOPSIS
+		Convert xCard to vCard
+
+	.DESCRIPTION
+		Convert input xCard (XML) to vCard (string)
+#>
+[CmdletBinding()]
+[OutputType( [System.String] )]
+
+Param(
+	# Исходный xCard
+	[Parameter( Mandatory = $True, ValueFromPipeline = $True )]
+	[ValidateScript( {
+			[System.Xml.Schema.XmlSchemaSet] $Schemas = New-Object System.Xml.Schema.XmlSchemaSet;
+			$xCardSchemaPath = ( Join-Path -Path $PSScriptRoot -ChildPath 'xCard.xsd' );
+			$Schemas.Add( 'urn:ietf:params:xml:ns:vcard-4.0', $xCardSchemaPath ) | Out-Null; ;
+			$_.Schemas = $Schemas;
+			# try
+			# {
+			$_.Validate( $null );
+			return $true;
+			# }
+			# catch
+			# {
+			# 	return $false;
+			# };
+		} ) ]
+	[System.Xml.XmlDocument]
+	$Input,
+
+	# Версия формата vCard
+	[Parameter( Mandatory = $False )]
+	[System.Int16]
+	$Version = 4
+)
+
+process
+{
+	$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
+
+	return '';
+}
