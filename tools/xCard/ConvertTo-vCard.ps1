06f2aa1ee1c2b887d3bfdf8a3a67161d04db5af0
Sergey S. Betke <sergey.s.betke@yandex.ru>
2021-05-22 21:15:54 +0300

    refactor(vcard): :recycle:
    #118

--- tools/xCard/ConvertTo-vCard.ps1
+++ tools/xCard/ConvertTo-vCard.ps1
@@ -1,4 +1,4 @@
-# Copyright © 2020 Sergei S. Betke
+﻿# Copyright © 2020 Sergei S. Betke
 
 #Requires -Version 5.0
 
@@ -44,7 +44,7 @@ begin
 {
 	$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
 
-	[System.Xml.Schema.XmlSchemaSet] $Schemas = New-Object System.Xml.Schema.XmlSchemaSet;
+	[System.Xml.Schema.XmlSchemaSet] $Schemas = [System.Xml.Schema.XmlSchemaSet]::new();
 	$xCardSchemaPath = ( Join-Path -Path $PSScriptRoot -ChildPath 'xsd/xCard.xsd' );
 	$Schemas.Add( 'urn:ietf:params:xml:ns:vcard-4.0', $xCardSchemaPath ) | Out-Null;
 
@@ -67,16 +67,18 @@ process
 	$saxTransform = $saxExecutable.Load30();
 
 	$xCard.Schemas = $Schemas;
-	$xCardStream = New-Object System.IO.MemoryStream;
-	$xCardStreamWriter = New-Object System.IO.StreamWriter -ArgumentList $xCardStream;
+	$xCardStream = [System.IO.MemoryStream]::new();
+	$xCardStreamWriter = [System.IO.StreamWriter]::new( $xCardStream, [System.Text.Encoding]::UTF8 );
 	$xCardStreamWriter.Write( $xCard.InnerXml );
 	$xCardStreamWriter.Flush();
 	$xCardStream.Position = 0;
 
-	$vCardSerializer = New-Object Saxon.Api.Serializer;
-	$vCardWriter = New-Object System.IO.StringWriter;
-	$vCardSerializer.SetOutputWriter( $vCardWriter );
+	$vCardStreamWriter = [System.IO.StringWriter]::new();
+	$vCardSerializer = [Saxon.Api.Serializer]::new();
+	$vCardSerializer.SetOutputWriter( $vCardStreamWriter );
+
 	$saxTransform.ApplyTemplates( $xCardStream, $vCardSerializer );
 
-	return $vCardWriter.ToString();
+	$vCardStreamWriter.Flush();
+	return $vCardStreamWriter.ToString();
 }
