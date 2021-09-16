b89e9798615c7a64276233c2c982015c245fb7dc
Sergey S. Betke <sergey.s.betke@yandex.ru>
2021-09-17 00:44:58 +0300

    test: :white_check_mark: Load ODFValidator with maven and pom.xml
    #159

---
+++ tests/Prepare-ODFValidator.ps1
@@ -0,0 +1,13 @@
+﻿#Requires -Version 5.0
+
+[System.String] $ODFValidatorPath = ( Join-Path -Path $PSScriptRoot -ChildPath 'java/dependency' );
+try
+{
+	Push-Location -Path $PSScriptRoot;
+	. mvn dependency:copy-dependencies;
+}
+finally
+{
+	Pop-Location;
+};
+[System.String] $ODFValidatorJarPath = ( Get-ChildItem -LiteralPath $ODFValidatorPath -Filter 'ODFValidator-*.jar' -File )[0].FullName;
